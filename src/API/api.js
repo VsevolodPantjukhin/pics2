5e8a89611417c5b2b55f3e0c9be31756afa7702e
haki_ainz <seva.pantiukhin@yandex.ru>
2023-10-19 14:35:39 +0400

    Data Fetching Cleanup

--- src/API/api.js
+++ src/API/api.js
@@ -1,16 +1,15 @@
 import axios from 'axios';
 
-const searchImages = async () => {
+const searchImages = async (term) => {
   const response = await axios.get('https://api.unsplash.com/search/photos', {
     headers: {
       Authorization: 'Client-ID lY8YoiucpXl1Fd2A_nQnuo6ly316Hx5U0s8oVAQLfo4',
     },
     params: {
-      query: 'cars',
+      query: term,
     },
   });
-  console.log(response);
-  return response;
+  return response.data.results;
 };
 
 export default searchImages;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App/App';
-import searchImages from './API/api';
-
-searchImages();
 
 const el = document.getElementById('root');
 const root = ReactDOM.createRoot(el);
+
 root.render(<App />);
