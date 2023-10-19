ee7d28064dd3d14cd792621075fc41cf81e46091
haki_ainz <seva.pantiukhin@yandex.ru>
2023-10-19 13:54:49 +0400

    Project Setup

---
+++ src/App/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import ImageList from '../components/ImageList/ImageList';
+import SearchBar from '../components/SearchBar/SearchBar';
+
+const App = () => {
+  return (
+    <div>
+      <SearchBar />
+      <ImageList />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/ImageList/ImageList.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import ImageShow from '../ImageShow/ImageShow';
+
+const ImageList = () => {
+  return (
+    <div>
+      <ImageShow />
+    </div>
+  );
+};
+
+export default ImageList;
